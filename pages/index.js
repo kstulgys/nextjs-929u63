3855f0ed4dfaefd944cb6ef4e5752d1fd80ede39
kstulgys <k.stulgys@gmail.com>
2021-08-14 10:26:02 +0300

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,47 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React from 'react';
 import { Button } from '@chakra-ui/react';
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFViewer
+} from '@react-pdf/renderer';
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+const MyPdf = () => (
+  <PDFViewer>
+    <MyDocument />
+  </PDFViewer>
+);
 
 // 3508 x 2480
 
